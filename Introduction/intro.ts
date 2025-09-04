62d1c1bbaacb9f733c290d6fab4d3fae9495ac48
Hanzala Khattak <muhammadhanzalahkhan1@gmail.com>
2025-09-04 11:30:54 +0500

    add variableMe.ts file and ran it.

--- Introduction/intro.ts
+++ Introduction/intro.ts
@@ -7,3 +7,12 @@ let user = {
 
 console.log("Hanzala");
 console.log(user.email);
+
+
+/// Types in TypeScript
+
+// 1) Number 2) String 3) Boolean
+// 4) Null   5) Undefined  6)Void
+// 7) Object 8) Array      9) Tuples
+// 10) Never 11) unknown
+// 12) Any => used when you're not sure what type to use here in TypeScript .... and more 
---
+++ Introduction/variableMe.ts
@@ -0,0 +1,10 @@
+let greetings : String = "Hello Atif!";
+greetings.toLocaleLowerCase();
+
+//// if you declared the type of the variable as string and put a dot in the end of it like 
+// "greetings. " then it will show all of the strings methods which can be used with this type variable
+
+
+console.log(greetings);
+
+export {};
