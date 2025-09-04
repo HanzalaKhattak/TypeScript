28f14fa29e77fd877fa58ef9b7714232450d0392
Hanzala Khattak <muhammadhanzalahkhan1@gmail.com>
2025-09-04 11:04:30 +0500

    Add user object and console logs in intro.js and intro.ts

---
+++ Introduction/intro.js
@@ -0,0 +1,6 @@
+var user = {
+    name: "Hanzala",
+    age: 22
+};
+console.log("Hanzala");
+console.log(user.email);
---
+++ Introduction/intro.ts
@@ -0,0 +1,9 @@
+// It saves time by tell what and where is the error while coding.
+
+let user = { 
+    name : "Hanzala",
+    age : 22
+}
+
+console.log("Hanzala");
+console.log(user.email);
